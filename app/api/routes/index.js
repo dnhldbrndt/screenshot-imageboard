ce05dff475f31c0b11a378f6d42099ef4628f4c9
Daniel Hildebrandt <48860201+dnhldbrndt@users.noreply.github.com>
2024-07-11 16:27:04 +0200

    Update index.js

--- app/api/routes/index.js
+++ app/api/routes/index.js
@@ -1,23 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
- 
-
-// Get the screenshot list available in the db
 router.get('/',function (req, res) {
-  //Write your code here
   return res.send("Nothing here but us trees.");
 });
- 
 
 router.get("/test", (req, res) => {
-  res.json({ message: "I tell him to eat the cookie beacuse it's good form."});
+  res.json({ message: "Testing."});
 });
-
-
- 
-
-
-
 
 module.exports = router;
